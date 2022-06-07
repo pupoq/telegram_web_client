6d687967a6bda73444c4e44fae4078f00e4e121e
Баталов Алмас Темирланулы <@lcoast.itstep>
2022-06-07 17:51:55 +0600

    put added

--- routers/usersRouter.js
+++ routers/usersRouter.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const userRouter = express.Router();
 const models = require('./../Models.js');
-const telegramBot = require('../modules/telegramBot')
+const telegramBot = require('../modules/telegramBot');
+const { model } = require("mongoose");
 
 userRouter.get('/', (req, res) => {
     models.User.find({}, (err, results) => {
@@ -9,6 +10,13 @@ userRouter.get('/', (req, res) => {
         res.send(results)
         console.log(results)
     })
+})
+
+userRouter.put('/', async (req, res) => {
+    const {_id, fullName, chatId} = req.body
+
+    await models.User.findByIdAndUpdate(_id, {fullName, chatId})
+    res.status(200).send("User updated")
 })
 
 userRouter.post('/sendMessage', (req, res) => {
